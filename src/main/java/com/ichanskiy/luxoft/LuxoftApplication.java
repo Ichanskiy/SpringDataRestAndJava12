8808bc3dabe8811d59b36b8460be3abcd5164154
Ichanskiy <ichanskiy.olexandr@gmail.com>
2019-10-07 21:32:04 +0300

    Initial commit

---
+++ src/main/java/com/ichanskiy/luxoft/LuxoftApplication.java
@@ -0,0 +1,13 @@
+package com.ichanskiy.luxoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuxoftApplication.class, args);
+    }
+
+}
